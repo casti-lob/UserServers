757fe6c37c858644e2036447044612b80a32f9b8
casti-lob <casti_lob@hotmail.com>
2022-12-05 12:21:43 +0100

    first commit

---
+++ src/app/users/componentesusers/componentesusers.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componentesusers',
+  templateUrl: './componentesusers.component.html'
+})
+export class ComponentesusersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/users/users.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponentesusersComponent } from './componentesusers/componentesusers.component';
+
+
+
+@NgModule({
+  declarations: [
+    ComponentesusersComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UsersModule { }
